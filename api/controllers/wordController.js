7ae80249bc1bcb287ca3187c05444ac6f783db3d
April Polubiec <aprilpolubiec@gmail.com>
2020-06-28 09:53:18 -0400

    wipe clean, new create-react-app

--- api/controllers/wordController.js
+++
@@ -1,9 +0,0 @@
-var Word = require('../models/word');
-
-exports.word_detail = function(req, res, next) {
-    Word.findById(req.params.id)
-        .populate("phonemes")
-        .exec(function(err, word) {
-            res.render('word_detail', { title: word.word, word: word });
-    })
-}
