da294812b37851bfa116c19203053f65d83b6392
April Polubiec <aprilpolubiec@gmail.com>
2020-07-14 18:32:23 -0500

    playing with paper

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,22 @@
 import * as React from "react";
 import Container from "@material-ui/core/Container";
+import { makeStyles } from "@material-ui/core/styles";
+
+import Paper from "@material-ui/core/Paper";
+import { Typography } from "@material-ui/core";
 
 import LessonList from "../components/LessonList";
 
 const Home = () => {
   return (
     <Container maxWidth="sm">
-      Hello from Home
+      {/* <div className={classes.root}>
+        <Paper elevation={3}>
+          <Typography variant="h6" component="h6">
+            Welcome Back!
+          </Typography>
+        </Paper>
+      </div> */}
       <LessonList />
     </Container>
   );
