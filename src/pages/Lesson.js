95ea32ff6d85a5d388886b7028b9afa4df90eee0
April Polubiec <aprilpolubiec@gmail.com>
2020-07-14 20:29:08 -0500

    remove unused vars

--- src/pages/Lesson.js
+++ src/pages/Lesson.js
@@ -1,14 +1,8 @@
 import React from "react";
-import { useParams } from "react-router-dom";
 import Keyboard from "../components/Keyboard";
-import { Container, Breadcrumbs, Link } from "@material-ui/core";
+import { Container } from "@material-ui/core";
 
 export default function Lesson() {
-  const { lesson } = useParams();
-  const handleClick = (event) => {
-    event.preventDefault();
-    console.info("You clicked a breadcrumb.");
-  };
   return (
     <>
       <Container maxWidth="sm">
