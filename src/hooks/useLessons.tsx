79dbbc18a576cfa82bca1ef13a8314b306681041
April Polubiec <aprilpolubiec@gmail.com>
2020-07-17 07:31:22 -0500

    sort lessons

--- src/hooks/useLessons.tsx
+++ src/hooks/useLessons.tsx
@@ -12,13 +12,38 @@ export function useLessons(): {
   const { loading } = useGetAllLessonsQuery({
     onCompleted: (data: GetAllLessonsQuery) => {
       console.log("Completed: ", data);
+
       if (data?.lessons) {
-        setLessons(data.lessons as Lesson[]);
+        //Sort lessons
+        const lessons = data.lessons.sort((a, b) => {
+          let aLessonID: string = a?.lesson_id!;
+          let bLessonID: string = b?.lesson_id!;
+          function isNumber(char: string) {
+            return char >= "0" && char <= "9";
+          }
+          if (isNumber(aLessonID.charAt(0)) && isNumber(bLessonID.charAt(0))) {
+            return parseFloat(aLessonID) - parseFloat(bLessonID);
+          } else if (
+            isNumber(aLessonID.charAt(0)) &&
+            !isNumber(bLessonID.charAt(0))
+          ) {
+            return -1;
+          } else if (
+            !isNumber(aLessonID.charAt(0)) &&
+            isNumber(bLessonID.charAt(0))
+          ) {
+            return 1;
+          } else {
+            return 0;
+          }
+        });
+        console.log(lessons);
+        setLessons(lessons as Lesson[]);
       }
     },
     onError: (error) => {
-      console.log("Error getting lessons: ", error)
-    }
+      console.log("Error getting lessons: ", error);
+    },
   });
 
   return {
