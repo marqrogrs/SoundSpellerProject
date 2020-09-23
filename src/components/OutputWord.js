57c14eb5dc4eb183582d6706affe4d0f71c03fef
April Polubiec <aprilpolubiec@gmail.com>
2020-09-22 21:27:09 -0500

    fix outputword

--- src/components/OutputWord.js
+++ src/components/OutputWord.js
@@ -8,7 +8,7 @@ import {
   speakPhoneme,
   SPEECH_RATE,
 } from '../util/Audio'
-import { useWords } from '../hooks/useWords'
+import { db } from '../firebase'
 import simulateEvent from 'simulate-event'
 
 const useStyles = makeStyles({
@@ -20,7 +20,6 @@ const useStyles = makeStyles({
 
 export default function OutputWord({ wordString, index }) {
   const classes = useStyles()
-  const { word } = useWords(wordString)
   const { currentLevel } = useContext(LessonContext)
 
   const renderKeyPress = (key) => {
@@ -54,74 +53,81 @@ export default function OutputWord({ wordString, index }) {
   }
 
   useEffect(() => {
-    console.log('OutputWord: ', wordString, index, word)
-    if (word && word.word) {
-      switch (currentLevel) {
-        case 0:
-          speakWord(word, index === 0).then(async () => {
-            let i = 0
-            for (const phoneme of word.phonemes) {
-              await speakPhoneme(phoneme)
-              await renderKeyPress(word.graphemes[i].toLowerCase())
-              i++
-            }
-            if (word.phonemes.length < word.graphemes.length) {
-              for (
-                let i = word.phonemes.length; // start where we left off
-                i < word.graphemes.length;
-                ++i
-              ) {
-                await renderKeyPress(word.graphemes[i].toLowerCase())
-              }
-            }
-            await playStartBells()
-            simulateEvent.simulate(document.body, 'keydown', {
-              key: 'esc',
-            })
-          })
-          break
-        case 1:
-          speakWord(word, index === 0).then(async () => {
-            let i = 0
-            for (const phoneme of word.phonemes) {
-              await speakPhoneme(phoneme)
-              await pressKey(word.graphemes[i].toLowerCase())
-              unpressKey(word.graphemes[i].toLowerCase())
-              i++
-            }
-            await playStartBells()
-            simulateEvent.simulate(document.body, 'keydown', {
-              key: 'esc',
-            })
-          })
-          break
-        case 2:
-          speakWord(word, index === 0).then(async () => {
-            for (const phoneme of word.phonemes) {
-              await speakPhoneme(phoneme)
-            }
-            setTimeout(async () => {
-              await playStartBells()
-              simulateEvent.simulate(document.body, 'keydown', {
-                key: 'esc',
+    // console.log('OutputWord: ', wordString, index)
+    db.collection('words')
+      .doc(wordString)
+      .get()
+      .then((wordDoc) => {
+        if (wordDoc.exists) {
+          const { word, phonemes, graphemes, syllables } = wordDoc.data()
+          switch (currentLevel) {
+            case 0:
+              speakWord(word, index === 0).then(async () => {
+                let i = 0
+                for (const phoneme of phonemes) {
+                  await speakPhoneme(phoneme)
+                  await renderKeyPress(graphemes[i].toLowerCase())
+                  i++
+                }
+                if (phonemes.length < graphemes.length) {
+                  for (
+                    let i = phonemes.length; // start where we left off
+                    i < graphemes.length;
+                    ++i
+                  ) {
+                    await renderKeyPress(graphemes[i].toLowerCase())
+                  }
+                }
+                await playStartBells()
+                simulateEvent.simulate(document.body, 'keydown', {
+                  key: 'esc',
+                })
               })
-            }, 1000)
-          })
-          break
-        case 3:
-          speakWord(word, index === 0).then(() => {
-            setTimeout(async () => {
-              await playStartBells()
-            }, 1000)
-          })
-          break
-        default:
-          return
-      }
-    } else if (!word) {
-      console.log('Not a word...')
-    }
-  }, [word])
+              break
+            case 1:
+              speakWord(word, index === 0).then(async () => {
+                let i = 0
+                for (const phoneme of phonemes) {
+                  await speakPhoneme(phoneme)
+                  await pressKey(graphemes[i].toLowerCase())
+                  unpressKey(graphemes[i].toLowerCase())
+                  i++
+                }
+                await playStartBells()
+                simulateEvent.simulate(document.body, 'keydown', {
+                  key: 'esc',
+                })
+              })
+              break
+            case 2:
+              speakWord(word, index === 0).then(async () => {
+                for (const phoneme of word.phonemes) {
+                  await speakPhoneme(phoneme)
+                }
+                setTimeout(async () => {
+                  await playStartBells()
+                  simulateEvent.simulate(document.body, 'keydown', {
+                    key: 'esc',
+                  })
+                }, 1000)
+              })
+              break
+            case 3:
+              speakWord(word, index === 0).then(() => {
+                setTimeout(async () => {
+                  await playStartBells()
+                }, 1000)
+              })
+              break
+            default:
+              return
+          }
+        } else {
+          console.log('Not a word')
+          // TODO: output graphemes
+        }
+      })
+  }, [wordString])
 
   return (
     <>
