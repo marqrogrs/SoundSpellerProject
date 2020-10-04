d4efebe52209ad7afeb8e77b20ffaf6ccb11dafe
April Polubiec <aprilpolubiec@gmail.com>
2020-10-04 15:01:43 -0500

    fix iseductator bug

--- src/providers/UserProvider.js
+++ src/providers/UserProvider.js
@@ -7,7 +7,7 @@ import { createStudentAccount } from '../firebase'
 const UserContext = React.createContext({})
 
 export default function UserProvider({ children }) {
-  const { user, isEducator, authLoaded } = useAuth()
+  const { user, authLoaded } = useAuth()
   const history = useHistory()
   const [userData, setUserData] = useState(null)
   const [classrooms, setClassrooms] = useState(null)
@@ -15,7 +15,8 @@ export default function UserProvider({ children }) {
 
   useEffect(() => {
     if (authLoaded && user) {
-      console.log('isEducator: ', isEducator)
+      const isEducator = user.email !== null
+      console.log(isEducator)
       const userDoc = isEducator
         ? db.collection('users').doc(user.uid)
         : db.collection('users').where('username', '==', user.uid)
