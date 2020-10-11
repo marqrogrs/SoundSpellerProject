a1a665ddd5dfdd61ec00cf2819ec373c585f2e0a
April Polubiec <aprilpolubiec@gmail.com>
2020-10-11 12:02:26 -0500

    do not enableinput during output

--- src/pages/Lesson.js
+++ src/pages/Lesson.js
@@ -95,7 +95,6 @@ export default function Lesson() {
       case 'shift':
         if (simulated) {
           setInputWord(inputWord + ' ')
-          setEnableInput(true)
         }
         break
       case 'tab':
