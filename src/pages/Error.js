44a3731f4b8a68aeaa5d1f5c323d239cd49235e5
April Polubiec <aprilpolubiec@gmail.com>
2020-11-15 10:39:57 -0600

    update error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,5 +1,26 @@
-import React from "react";
+import React from 'react'
+import { Typography } from '@material-ui/core'
+import { Link } from '@material-ui/core'
+import { useHistory } from 'react-router-dom'
+
+var Snake = require('../img/Welcome.png')
 
 export default function Error() {
-  return <div>Error!</div>;
+  const history = useHistory()
+
+  return (
+    <div id='error-container'>
+      <img src={Snake} />
+      <div className='right-panel'>
+        <Typography className='upper-text'>Oops!</Typography>
+        <Typography className='lower-text'>
+          The page you are looking for does not exist.{' '}
+          <Link onClick={() => history.push('/')}>Click here</Link> to go back
+          home or{' '}
+          <Link onClick={() => history.push('/contact-us')}>contact us</Link>{' '}
+          for help.
+        </Typography>
+      </div>
+    </div>
+  )
 }
