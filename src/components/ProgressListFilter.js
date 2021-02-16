654694fadd515ed7c71c8f47581c0c5dad13a103
April Polubiec <aprilpolubiec@gmail.com>
2021-02-16 15:10:12 -0600

    add todo for j

--- src/components/ProgressListFilter.js
+++ src/components/ProgressListFilter.js
@@ -1,5 +1,14 @@
 import React from 'react'
 
+//TODO:@J Add two checkboxes for `Custom Lesson`.
+// - one should be labeled "Custom"
+// - one should be labeled "Standard"
+// This way, a user can choose to see only Custom, only Standard or all lessons
+// When the `Custom` checkbox is checked, a state variable (hint: useState()) called `showCustomLessons` should be set to True
+// When the `Custom` checkbox is unchecked, a state variable (hint: useState()) called `showCustomLessons` should be set to False
+// When the `Standard` checkbox is checked, a state variable (hint: useState()) called `showStandardLessons` should be set to True
+// When the `Standard` checkbox is unchecked, a state variable (hint: useState()) called `showStandardLessons` should be set to False
+
 export default function ProgressListFilter() {
     return (
         <div>
