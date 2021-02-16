1364a9ba12daad83d931dcac8037db3e583bdbc8
April Polubiec <aprilpolubiec@gmail.com>
2021-02-16 14:47:52 -0600

    created dummy progress list filter

---
+++ src/components/ProgressListFilter.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function ProgressListFilter() {
+    return (
+        <div>
+            <div>
+                Lesson Number
+            </div>
+            <div>
+                Spelling Patterns
+            </div>
+            <div>
+                Level Completed
+            </div>
+            <div>
+                Custom Lesson/ Or Not
+            </div>
+            <div>
+                Lesson Score
+            </div>
+            <div>
+                Assigned Lessons
+            </div>
+        </div>
+    )
+}
