4aabf46d2d7dc915ed0f22f8fff8731b501eb33f
April Polubiec <aprilpolubiec@gmail.com>
2021-02-16 15:12:19 -0600

    add more TODOs

--- src/components/ProgressListFilter.js
+++ src/components/ProgressListFilter.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-//TODO:@J Add two checkboxes for `Custom Lesson`.
+//TODO:@J Add two checkboxes for `Custom Lesson`. (https://material-ui.com/components/checkboxes/)
 // - one should be labeled "Custom"
 // - one should be labeled "Standard"
 // This way, a user can choose to see only Custom, only Standard or all lessons
@@ -9,6 +9,8 @@ import React from 'react'
 // When the `Standard` checkbox is checked, a state variable (hint: useState()) called `showStandardLessons` should be set to True
 // When the `Standard` checkbox is unchecked, a state variable (hint: useState()) called `showStandardLessons` should be set to False
 
+//TODO:@J continue fleshing out these filter options. Browse through the different `Input` components in MaterialUi (https://material-ui.com/components)
+// for inspo on what types of inputs you want to use. Each input should be hooked up to a state variable like the checkboxes.
 export default function ProgressListFilter() {
     return (
         <div>
