1664a9fe30535191d43434a2af79287c2fabd15c
jordangibson1 <gibsonj22@hanover.edu>
2021-03-25 01:40:34 -0400

    Beginning to setup checkboxes

--- src/components/ProgressListFilter.js
+++ src/components/ProgressListFilter.js
@@ -1,4 +1,9 @@
 import React from 'react'
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+
+
 
 //TODO:@J Add two checkboxes for `Custom Lesson`. (https://material-ui.com/components/checkboxes/)
 // - one should be labeled "Custom"
@@ -11,8 +16,45 @@ import React from 'react'
 
 //TODO:@J continue fleshing out these filter options. Browse through the different `Input` components in MaterialUi (https://material-ui.com/components)
 // for inspo on what types of inputs you want to use. Each input should be hooked up to a state variable like the checkboxes.
-export default function ProgressListFilter() {
-    return (
+
+export default function CheckboxLabels() {
+  const [state, setState] = React.useState({
+    const [checkedA, setcheckedA] = useState(true),
+    checkedB: true,
+    checkedC: true,
+    checkedD: true,
+  });
+
+  const handleChange = (event) => {
+
+    setState({ ...state, [event.target.name === "checkedA"]: event.target.checked });
+  };
+
+  return (
+    <FormGroup row>
+      <FormControlLabel
+        control={<Checkbox checked={checkedA} onChange={handleChange} name="checkedA" />}
+        label="A"
+      />
+
+      <FormControlLabel
+        control={<Checkbox checked={state.checkedA} onChange={handleChange} name="checkedB" />}
+        label="B"
+      />
+
+      <FormControlLabel
+        control={<Checkbox checked={state.checkedA} onChange={handleChange} name="checkedC" />}
+        label="C"
+      />
+
+      <FormControlLabel
+        control={<Checkbox checked={state.checkedA} onChange={handleChange} name="checkedD" />}
+        label="D"
+      />
+    </FormGroup>
+  );
+}
+/*}
         <div>
             <div>
                 Lesson Number
@@ -34,4 +76,4 @@ export default function ProgressListFilter() {
             </div>
         </div>
     )
-}
+} */
