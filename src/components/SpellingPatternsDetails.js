b0716a20bbd4db9b3e581f35fc9fd39da83cbf06
Pedro Almeida <59484474+PedroMarianoAlmeida@users.noreply.github.com>
2021-04-12 10:00:52 -0300

    Button who goes to lesson created

--- src/components/SpellingPatternsDetails.js
+++ src/components/SpellingPatternsDetails.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 import Accordion from "@material-ui/core/Accordion";
 import AccordionSummary from "@material-ui/core/AccordionSummary";
@@ -6,9 +7,11 @@ import AccordionDetails from "@material-ui/core/AccordionDetails";
 import Typography from "@material-ui/core/Typography";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Button";
 
 const SpellingPatternsDetails = ({ rule, lessons }) => {
   console.log(lessons);
+  const history = useHistory();
 
   return (
     <Accordion>
@@ -27,9 +30,22 @@ const SpellingPatternsDetails = ({ rule, lessons }) => {
               </AccordionSummary>
               <AccordionDetails>
                 {lessons.map((lesson) => (
-                  <Typography key={lesson.lesson_id}>
-                    {lesson.description}
-                  </Typography>
+                  <>
+                    <Typography key={lesson.lesson_id}>
+                      {lesson.description}
+                    </Typography>
+                    <Box ml={2}>
+                      <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={() =>
+                          history.push(`/lessons/${lesson.lesson_id}`)
+                        }
+                      >
+                        Go to lesson
+                      </Button>
+                    </Box>
+                  </>
                 ))}
               </AccordionDetails>{" "}
             </Accordion>
