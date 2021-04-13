7172f9569e75f1af419c3c6d6d3a15dc0cdb3498
April Polubiec <aprilpolubiec@gmail.com>
2021-04-13 13:42:26 -0500

    only speak individual levels on section 1

--- src/components/OutputWord.js
+++ src/components/OutputWord.js
@@ -1,7 +1,12 @@
-import { Typography } from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles'
-import React, { useEffect, useContext, useState, useRef } from 'react'
-import { LessonContext } from '../providers/LessonProvider'
+import { Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import React, {
+  useEffect,
+  useContext,
+  useState,
+  useRef,
+} from 'react';
+import { LessonContext } from '../providers/LessonProvider';
 import {
   speakWord,
   playStartBells,
@@ -9,64 +14,64 @@ import {
   SPEECH_RATE,
   terminateAudio,
   setPlayAudio,
-} from '../util/Audio'
-import { COMMON_PHONEMES } from '../util/constants'
-import { db } from '../firebase'
-import simulateEvent from 'simulate-event'
+} from '../util/Audio';
+import { COMMON_PHONEMES } from '../util/constants';
+import { db } from '../firebase';
+import simulateEvent from 'simulate-event';
 
 const useStyles = makeStyles({
   word: {
     color: '#002ca0',
     alignSelf: 'center',
   },
-})
+});
 
 export default function OutputWord({ wordString, index }) {
-  const classes = useStyles()
-  const { currentLesson } = useContext(LessonContext)
+  const classes = useStyles();
+  const { currentLesson } = useContext(LessonContext);
 
   const renderKeyPress = (key) => {
     return new Promise(async (resolve, reject) => {
       for (let i = 0; i < Array.from(key).length; ++i) {
-        await pressKey(Array.from(key)[i])
-        await unpressKey(Array.from(key)[i])
+        await pressKey(Array.from(key)[i]);
+        await unpressKey(Array.from(key)[i]);
       }
-      await insertSpaceAfterLetter()
-      resolve()
-    })
-  }
+      await insertSpaceAfterLetter();
+      resolve();
+    });
+  };
 
   const insertSpaceAfterLetter = () => {
     return new Promise((resolve, reject) => {
       simulateEvent.simulate(document.body, 'keydown', {
         key: 'shift',
-      })
+      });
       simulateEvent.simulate(document.body, 'keyup', {
         key: 'shift',
-      })
-      resolve()
-    })
-  }
+      });
+      resolve();
+    });
+  };
 
   const pressKey = (key) => {
     return new Promise((resolve, reject) => {
       simulateEvent.simulate(document.body, 'keydown', {
         key,
-      })
+      });
       setTimeout(() => {
-        resolve()
-      }, 400 / SPEECH_RATE)
-    })
-  }
+        resolve();
+      }, 400 / SPEECH_RATE);
+    });
+  };
 
   const unpressKey = (key) => {
     return new Promise((resolve, reject) => {
       simulateEvent.simulate(document.body, 'keyup', {
         key,
-      })
-      resolve()
-    })
-  }
+      });
+      resolve();
+    });
+  };
 
   // const phonemePause = () => {
   //   return new Promise((resolve, reject) => {
@@ -79,56 +84,71 @@ export default function OutputWord({ wordString, index }) {
   const getGraphemesInSyllable = (
     syllable,
     graphemes,
-    lastKnownSyllableIndex
+    lastKnownSyllableIndex,
   ) => {
-    let i = lastKnownSyllableIndex
-    var graphemesInSyllable = []
+    let i = lastKnownSyllableIndex;
+    var graphemesInSyllable = [];
     while (graphemesInSyllable.join('').toLowerCase() !== syllable) {
-      graphemesInSyllable.push(graphemes[i])
-      i++
+      graphemesInSyllable.push(graphemes[i]);
+      i++;
     }
-    return { graphemesInSyllable, index: i }
-  }
+    return { graphemesInSyllable, index: i };
+  };
 
-  const getWordDataBySyllable = ({ syllables, graphemes, phonemes }) => {
+  const getWordDataBySyllable = ({
+    syllables,
+    graphemes,
+    phonemes,
+  }) => {
     //Break up the data by syllable
-    var wordDataBySyllable = []
-    let lastSyllableIndexGrapheme = 0
-    let lastSyllableIndexPhoneme = 0
+    var wordDataBySyllable = [];
+    let lastSyllableIndexGrapheme = 0;
+    let lastSyllableIndexPhoneme = 0;
 
     for (let i = 0; i < syllables.length; ++i) {
       //Get graphemes in syllable
       var { graphemesInSyllable, index } = getGraphemesInSyllable(
         syllables[i],
         graphemes,
-        lastSyllableIndexGrapheme
-      )
-      lastSyllableIndexGrapheme = index
+        lastSyllableIndexGrapheme,
+      );
+      lastSyllableIndexGrapheme = index;
 
       //Get phonemes in syllable
-      var syllableStart = lastSyllableIndexPhoneme
+      var syllableStart = lastSyllableIndexPhoneme;
       var syllableEnd =
         i === syllables.length - 1
           ? phonemes.length
-          : phonemes.indexOf('-', lastSyllableIndexPhoneme)
-      var phonemesInSyllable = phonemes.slice(syllableStart, syllableEnd)
+          : phonemes.indexOf('-', lastSyllableIndexPhoneme);
+      var phonemesInSyllable = phonemes.slice(
+        syllableStart,
+        syllableEnd,
+      );
 
-      lastSyllableIndexPhoneme = syllableEnd + 1
+      lastSyllableIndexPhoneme = syllableEnd + 1;
       wordDataBySyllable.push({
         phonemesInSyllable,
         graphemesInSyllable,
-      })
+      });
     }
-    return wordDataBySyllable
-  }
+    return wordDataBySyllable;
+  };
 
   useEffect(() => {
     db.collection('words')
       .doc(wordString)
       .get()
       .then(async (wordDoc) => {
-        if (wordDoc.exists && currentLesson.lesson.lesson_section > 1) {
-          var { word, phonemes, graphemes, syllables } = wordDoc.data()
+        if (
+          wordDoc.exists &&
+          currentLesson.lesson.lesson_section !== 1
+        ) {
+          var {
+            word,
+            phonemes,
+            graphemes,
+            syllables,
+          } = wordDoc.data();
           // xample:  ["D", "EY", "-", "T", "AH"]
           // ["D", "A", "T", "A"]
           // ["da", "ta"]
@@ -141,10 +161,10 @@ export default function OutputWord({ wordString, index }) {
                   phonemes,
                   graphemes,
                   syllables,
-                })
+                });
 
                 for (const syllable of wordDataBySyllable) {
-                  let i = 0
+                  let i = 0;
                   for (const grapheme of syllable.graphemesInSyllable) {
                     if (syllable.phonemesInSyllable[i]) {
                       // Handle special cases
@@ -152,107 +172,118 @@ export default function OutputWord({ wordString, index }) {
                         case 'U':
                           if (
                             syllable.phonemesInSyllable[i] === 'Y' &&
-                            syllable.phonemesInSyllable[i + 1] === 'UW'
+                            syllable.phonemesInSyllable[i + 1] ===
+                              'UW'
                           ) {
-                            await speakPhoneme(syllable.phonemesInSyllable[i])
                             await speakPhoneme(
-                              syllable.phonemesInSyllable[i + 1]
-                            )
-                            syllable.phonemesInSyllable[i] = 'YUW'
+                              syllable.phonemesInSyllable[i],
+                            );
+                            await speakPhoneme(
+                              syllable.phonemesInSyllable[i + 1],
+                            );
+                            syllable.phonemesInSyllable[i] = 'YUW';
                             syllable.phonemesInSyllable = syllable.phonemesInSyllable
                               .slice(0, i + 1)
-                              .concat(syllable.phonemesInSyllable.slice(i + 2))
+                              .concat(
+                                syllable.phonemesInSyllable.slice(
+                                  i + 2,
+                                ),
+                              );
                           } else {
-                            await speakPhoneme(syllable.phonemesInSyllable[i])
+                            await speakPhoneme(
+                              syllable.phonemesInSyllable[i],
+                            );
                           }
-                          break
+                          break;
                         default:
-                          await speakPhoneme(syllable.phonemesInSyllable[i])
+                          await speakPhoneme(
+                            syllable.phonemesInSyllable[i],
+                          );
                       }
                     }
-                    await renderKeyPress(grapheme.toLowerCase())
-                    i++
+                    await renderKeyPress(grapheme.toLowerCase());
+                    i++;
                   }
                   if (currentLesson.level === 0) {
                     simulateEvent.simulate(document.body, 'keydown', {
                       key: 'tab',
-                    })
+                    });
                   }
                 }
 
                 setTimeout(async () => {
-                  await playStartBells()
+                  await playStartBells();
                   simulateEvent.simulate(document.body, 'keydown', {
                     key: 'esc',
-                  })
-                }, 500)
-              })
-              break
+                  });
+                }, 500);
+              });
+              break;
             case 2:
               speakWord(word, index === 0).then(async () => {
                 for (const phoneme of word.phonemes) {
-                  await speakPhoneme(phoneme)
+                  await speakPhoneme(phoneme);
                 }
                 setTimeout(async () => {
-                  await playStartBells()
+                  await playStartBells();
                   simulateEvent.simulate(document.body, 'keydown', {
                     key: 'esc',
-                  })
-                }, 1000)
-              })
-              break
+                  });
+                }, 1000);
+              });
+              break;
             case 3:
               speakWord(word, index === 0).then(() => {
                 setTimeout(async () => {
-                  await playStartBells()
-                }, 1000)
-              })
-              break
+                  await playStartBells();
+                }, 1000);
+              });
+              break;
             default:
-              return
+              return;
           }
         } else {
-          const graphemes = wordString.split('')
+          const graphemes = wordString.split('');
           const phonemes = graphemes.map(
-            (g) => COMMON_PHONEMES[g.toLowerCase()]
-          )
+            (g) => COMMON_PHONEMES[g.toLowerCase()],
+          );
           switch (currentLesson.level) {
             case 0:
             case 1:
-              let i = 0
+              let i = 0;
               for (const phoneme of phonemes) {
-                await speakPhoneme(phoneme)
-                await renderKeyPress(graphemes[i].toLowerCase())
-                i++
+                await speakPhoneme(phoneme);
+                await renderKeyPress(graphemes[i].toLowerCase());
+                i++;
               }
-              break
+              break;
             case 2:
               //TODO: how do we handle level 4 ?
               for (const phoneme of phonemes) {
-                await speakPhoneme(phoneme)
+                await speakPhoneme(phoneme);
               }
-              break
+              break;
             default:
-              return
+              return;
           }
           setTimeout(async () => {
-            await playStartBells()
+            await playStartBells();
             simulateEvent.simulate(document.body, 'keydown', {
               key: 'esc',
-            })
-          }, 1000)
+            });
+          }, 1000);
         }
-      })
-  }, [wordString])
+      });
+  }, [wordString]);
 
   useEffect(() => {
-    setPlayAudio(true)
-    return terminateAudio
-  }, [])
+    setPlayAudio(true);
+    return terminateAudio;
+  }, []);
 
   return (
     <>
-      <Typography className={classes.word} variant='h1'></Typography>
+      <Typography className={classes.word} variant="h1"></Typography>
     </>
-  )
+  );
 }
