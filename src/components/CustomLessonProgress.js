b8b9237e2c2d97800c9dc1e8792d34c7c1054a8a
April Polubiec <aprilpolubiec@gmail.com>
2021-04-13 09:07:12 -0500

    check for lesson progress

--- src/components/CustomLessonProgress.js
+++ src/components/CustomLessonProgress.js
@@ -1,59 +1,64 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react';
 
-import Table from '@material-ui/core/Table'
-import TableBody from '@material-ui/core/TableBody'
-import TableCell from '@material-ui/core/TableCell'
-import TableContainer from '@material-ui/core/TableContainer'
-import TableHead from '@material-ui/core/TableHead'
-import TableRow from '@material-ui/core/TableRow'
-import Paper from '@material-ui/core/Paper'
-import { LessonContext } from '../providers/LessonProvider'
-import ProgressListItem from './ProgressListItem'
-import Tabs from '@material-ui/core/Tabs'
-import Tab from '@material-ui/core/Tab'
-import Typography from '@material-ui/core/Typography'
-import Box from '@material-ui/core/Box'
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import { LessonContext } from '../providers/LessonProvider';
+import ProgressListItem from './ProgressListItem';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 
-import { UserContext } from '../providers/UserProvider'
-import { getLessonSubsection } from '../util/functions'
-import { INIT_PROGRESS_OBJ } from '../util/constants'
-import { useStyles } from '../styles/material'
-import { db, auth } from '../firebase'
+import { UserContext } from '../providers/UserProvider';
+import { getLessonSubsection } from '../util/functions';
+import { INIT_PROGRESS_OBJ } from '../util/constants';
+import { useStyles } from '../styles/material';
+import { db, auth } from '../firebase';
 
-export default function CustomLessonProgress({ student, userProgress }) {
-  const { lessonsLoading, customLessons } = useContext(LessonContext)
-  const { userData } = useContext(UserContext)
-  const [customLessonProgress, setCustomLessonProgress] = useState(null)
-  const [selectedTab, setSelectedTab] = useState(0)
-  const classes = useStyles()
+export default function CustomLessonProgress({
+  student,
+  userProgress,
+}) {
+  const { lessonsLoading, customLessons } = useContext(LessonContext);
+  const { userData } = useContext(UserContext);
+  const [customLessonProgress, setCustomLessonProgress] = useState(
+    null,
+  );
+  const [selectedTab, setSelectedTab] = useState(0);
+  const classes = useStyles();
 
   useEffect(() => {
-    var progress = {}
-    if (!lessonsLoading) {
+    var progress = {};
+    if (!lessonsLoading && userProgress) {
       customLessons.forEach((customLesson) => {
         if (userProgress[customLesson.id]) {
-          progress[customLesson.id] = userProgress[customLesson.id]
+          progress[customLesson.id] = userProgress[customLesson.id];
         } else {
-          progress[customLesson.id] = INIT_PROGRESS_OBJ
+          progress[customLesson.id] = INIT_PROGRESS_OBJ;
         }
-      })
-      setCustomLessonProgress(progress)
+      });
+      setCustomLessonProgress(progress);
     }
-  }, [lessonsLoading])
+  }, [lessonsLoading, userProgress]);
 
   return (
     <>
-      <Typography variant='h3'>Custom Lessons</Typography>
+      <Typography variant="h3">Custom Lessons</Typography>
       <div className={classes.progressTabContainer}>
         <Tabs
           value={selectedTab}
           onChange={(e, selected) => setSelectedTab(selected)}
-          indicatorColor='primary'
-          textColor='primary'
-          variant='scrollable'
-          scrollButtons='on'
-          orientation='vertical'
-          aria-label='section-tabs'
+          indicatorColor="primary"
+          textColor="primary"
+          variant="scrollable"
+          scrollButtons="on"
+          orientation="vertical"
+          aria-label="section-tabs"
           className={classes.progressTabs}
         >
           {!lessonsLoading &&
@@ -70,7 +75,7 @@ export default function CustomLessonProgress({ student, userProgress }) {
             {customLessons.map((customLesson, i) => {
               return (
                 <div
-                  role='tabpanel'
+                  role="tabpanel"
                   hidden={selectedTab !== i}
                   id={`tab-${i}`}
                   aria-labelledby={`tabpanel-${i}`}
@@ -79,15 +84,17 @@ export default function CustomLessonProgress({ student, userProgress }) {
                   {selectedTab === i && (
                     <>
                       <Box p={3} maxWidth={700}>
-                        <Typography variant='h4'>
+                        <Typography variant="h4">
                           {customLesson.title}
                         </Typography>
-                        <Typography variant='subtitle1'>
+                        <Typography variant="subtitle1">
                           {customLesson.description}
                         </Typography>
                         <ProgressListItem
                           lesson={customLesson}
-                          progress={customLessonProgress[customLesson.id]}
+                          progress={
+                            customLessonProgress[customLesson.id]
+                          }
                           showButtons={student ? false : true}
                           key={i}
                           isOpen={true}
@@ -98,11 +105,11 @@ export default function CustomLessonProgress({ student, userProgress }) {
                     </>
                   )}
                 </div>
-              )
+              );
             })}
           </>
         )}
       </div>
     </>
-  )
+  );
 }
