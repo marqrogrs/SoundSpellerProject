15ed1ef261353bda43a061664b8869d90c535166
April Polubiec <aprilpolubiec@gmail.com>
2021-04-17 16:58:47 -0500

    create lesson page includes lesson sections

--- src/pages/CreateLesson.js
+++ src/pages/CreateLesson.js
@@ -18,13 +18,19 @@ import { LessonContext } from '../providers/LessonProvider';
 
 export default function CreateLesson() {
   const [lessonWords, setLessonWords] = useState('');
+  const [lessonSection, setLessonSection] = useState('');
   const [lessonTitle, setLessonTitle] = useState('');
   const [description, setDescription] = useState('');
   const [selectedRules, setSelectedRules] = useState([]);
   const [openRulesDropdown, setOpenRulesDropdown] = useState(false);
   const history = useHistory();
 
-  const { createLesson, rules } = useContext(LessonContext);
+  const {
+    createLesson,
+    customLessonSections,
+    rules,
+    lessonsLoading,
+  } = useContext(LessonContext);
 
   const handleSubmit = () => {
     const words = lessonWords
@@ -67,63 +73,82 @@ export default function CreateLesson() {
   return (
     <Container maxWidth="md">
       <Grid container spacing={2} direction="column">
-        <TextField
-          id="name"
-          label="Lesson Name"
-          value={lessonTitle}
-          onChange={(e) => setLessonTitle(e.target.value)}
-        />
-        <TextField
-          id="description"
-          label="Lesson Description"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-        />
-        <FormControl>
-          <InputLabel>Rules</InputLabel>
-          <Select
-            multiple
-            value={selectedRules}
-            onOpen={() => setOpenRulesDropdown(true)}
-            onChange={handleRulesChanged}
-            open={openRulesDropdown}
-            input={<Input />}
-            renderValue={(selected) => (
-              <div>
-                {selected.map((value) => (
-                  <Chip
-                    key={value}
-                    label={`${value}: ${rules[value].rule_title}`}
-                  />
-                ))}
-              </div>
-            )}
-          >
-            {Object.entries(rules).map(([id, ruleData]) => {
-              return (
-                <MenuItem key={id} value={id}>
-                  {`${id}: ${ruleData.rule_title}`}
-                </MenuItem>
-              );
-            })}
-          </Select>
-        </FormControl>
-        <TextField
-          id="words"
-          label="Lesson Words"
-          multiline
-          rows={20}
-          placeholder="Enter lesson words here"
-          value={lessonWords}
-          onChange={(e) => setLessonWords(e.target.value)}
-        />
-        <Button
-          color="primary"
-          variant="contained"
-          onClick={handleSubmit}
-        >
-          Create Lesson
-        </Button>
+        {!lessonsLoading && (
+          <>
+            <FormControl>
+              <InputLabel>Lesson Section</InputLabel>
+              <Select
+                value={lessonSection}
+                onChange={(e) => setLessonSection(e.target.value)}
+              >
+                {customLessonSections.map((section) => {
+                  return (
+                    <MenuItem key={section.id} value={section.id}>
+                      {section.title}
+                    </MenuItem>
+                  );
+                })}
+              </Select>
+            </FormControl>
+            <TextField
+              id="name"
+              label="Lesson Name"
+              value={lessonTitle}
+              onChange={(e) => setLessonTitle(e.target.value)}
+            />
+            <TextField
+              id="description"
+              label="Lesson Description"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+            <FormControl>
+              <InputLabel>Rules</InputLabel>
+              <Select
+                multiple
+                value={selectedRules}
+                onOpen={() => setOpenRulesDropdown(true)}
+                onChange={handleRulesChanged}
+                open={openRulesDropdown}
+                input={<Input />}
+                renderValue={(selected) => (
+                  <div>
+                    {selected.map((value) => (
+                      <Chip
+                        key={value}
+                        label={`${value}: ${rules[value].rule_title}`}
+                      />
+                    ))}
+                  </div>
+                )}
+              >
+                {Object.entries(rules).map(([id, ruleData]) => {
+                  return (
+                    <MenuItem key={id} value={id}>
+                      {`${id}: ${ruleData.rule_title}`}
+                    </MenuItem>
+                  );
+                })}
+              </Select>
+            </FormControl>
+            <TextField
+              id="words"
+              label="Lesson Words"
+              multiline
+              rows={20}
+              placeholder="Enter lesson words here"
+              value={lessonWords}
+              onChange={(e) => setLessonWords(e.target.value)}
+            />
+            <Button
+              color="primary"
+              variant="contained"
+              onClick={handleSubmit}
+            >
+              Create Lesson
+            </Button>
+          </>
+        )}
       </Grid>
     </Container>
   );
